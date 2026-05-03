3356872e3397ed77e7a897812fb6252552703876
adept <timofeysitnikov@mail.ru>
2026-05-03 10:51:42 +0300

    upd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -5,7 +5,7 @@ local highlight_group = ag('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
     callback = function()
-        vim.highlight.on_yank({ timeout = 170 })
+        vim.hl.on_yank({ timeout = 170 })
     end,
     group = highlight_group,
 })
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -5,3 +5,4 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<Leader>fo", ":lua vim.lsp.buf.format()<CR>", opts)
 vim.keymap.set('n', 'gb', vim.diagnostic.open_float)
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
